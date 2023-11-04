c1fde456fd57be2a052e769132eadb9a005ad72e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-04 06:17:58 +0200

    Event Details

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,24 @@
+import { useRouter } from "next/router";
+import { getEventById } from '../../dummy-data';
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventDetailPage() {
+  const router = useRouter();
+  //console.log(router.query.eventId);
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if(!event) return;
+
   return (
-    <div>
-      <h1>🎉 Event Detail Page 🎉</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.address} image={event.image} imageAlt={event.title} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
 
