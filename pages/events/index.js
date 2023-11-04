42423ab28ea6ec688473d6586028fe1cd2752a70
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-04 23:13:04 +0200

    Navigating

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,20 @@
 import {getAllEvents} from '../../dummy-data';
 import EventList from "../../components/events/EventList";
 import EventsSearch from '../../components/events/EventsSearch';
+import { useRouter } from 'next/router';
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function handleFindEvents(year, month){
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
   return (
     <div>
-      <EventsSearch/>
+      <EventsSearch onSearch={handleFindEvents}/>
       <EventList items={events} />
     </div>
   );
