34750c13e3bd2373a7f893208ab85775cfa6bfd9
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-04 23:57:17 +0200

    Update

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data";
-
+import EventList from "../../components/events/EventList";
+import ResultsTitle from "../../components/events/ResultsTitle";
+import Button from "../../components/ui/Button";
 function FilteredEventsPage() {
   const router = useRouter();
   // console.log(router.query.slug)
@@ -11,7 +13,10 @@ function FilteredEventsPage() {
   const filteredMonth = +filterData[1];
 
   if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredYear < 2021 || filteredMonth < 1 || filteredMonth > 12 ){
-    return <h1>Invalid Data</h1>
+    return <>
+    <h1>Invalid Data</h1>
+    <Button  link={'/events'}>Show All Events</Button>
+    </>
   }
   
   const filteredEvents = getFilteredEvents({
@@ -20,12 +25,17 @@ function FilteredEventsPage() {
   });
   
   if(!filteredEvents || filteredEvents.length === 0){
-    return <h1>No Events Yet</h1>
+    return <>
+      <h1>No Events Yet</h1>
+      <Button link={'/events'}>Show All Events</Button>
+    </>
   }
   
+  const date = new Date(filteredYear, filteredMonth - 1)
   return (
     <div>
-      <h1>🎉🎉 Filtered Events Page 🎉🎉 </h1>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
     </div>
   );
 }
