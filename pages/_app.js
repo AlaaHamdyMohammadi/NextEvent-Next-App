0aa433e250384f8ed47f4dd9902cc8b781adb60f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-09 00:16:00 +0200

    Merge Head Content

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return <Layout>
     <Head>
+      <title>NextJS Events</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
     </Head>
     <Component {...pageProps} />
