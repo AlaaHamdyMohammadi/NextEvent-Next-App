8dc9a2437cf717edf220f63357867ddbecf3ad26
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-08 02:18:10 +0200

    Adding SSG

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import EventList from "../components/events/EventList";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
+import ErrorAlert from '../components/ui/ErrorAlert';
+function HomePage(props){
+    const {featuredEvents} = props;
+    if (!featuredEvents || featuredEvents.length === 0){
+      return <ErrorAlert/>
+    }
 
-function HomePage(){
-    const featuredEvents = getFeaturedEvents();
-    return (
-      <div>
-        <EventList items={featuredEvents} />
-      </div>
-    );
+      return (
+        <div>
+          <EventList items={featuredEvents} />
+        </div>
+      );
 }
 
 export default HomePage;
+
+export async function getStaticProps(context){
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: { featuredEvents },
+    revalidate: 5,
+  };
+}
