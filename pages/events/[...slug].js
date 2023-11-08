b8013c86b24b14b6d24e105d248cc13de83b7176
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-08 19:05:52 +0200

    Handle SSR

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,29 +1,37 @@
 import { useRouter } from "next/router";
-import { getFilteredEvents } from "../../dummy-data";
+import { getFilteredEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/EventList";
 import ResultsTitle from "../../components/events/ResultsTitle";
 import Button from "../../components/ui/Button";
 import ErrorAlert from "../../components/ui/ErrorAlert";
-function FilteredEventsPage() {
-  const router = useRouter();
-  // console.log(router.query.slug)
-  const filterData = router.query.slug;
-  if (!filterData) return <h1 className="center">Loading..</h1>; //Like Spinner
-  
-  const filteredYear = +filterData[0];
-  const filteredMonth = +filterData[1];
 
-  if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredYear < 2021 || filteredMonth < 1 || filteredMonth > 12 ){
-    return <div className="center">
-    <ErrorAlert>Invalid Data</ErrorAlert>
-    <Button link={'/events'}>Show All Events</Button>
-    </div>
+
+function FilteredEventsPage(props) {
+  const {hasError, events, date: dateFromServer} = props;
+
+  // const router = useRouter();
+  // console.log(router.query.slug)
+  // const filterData = router.query.slug;
+  // if (!filterData) return <h1 className="center">Loading..</h1>; //Like Spinner
+  
+  // const filteredYear = +filterData[0];
+  // const filteredMonth = +filterData[1];
+
+  if (hasError) {
+    return (
+      <div className="center">
+        <ErrorAlert>Invalid Data</ErrorAlert>
+        <Button link={"/events"}>Show All Events</Button>
+      </div>
+    );
   }
   
-  const filteredEvents = getFilteredEvents({
-    year: filteredYear,
-    month: filteredMonth,
-  });
+  // const filteredEvents = getFilteredEvents({
+  //   year: filteredYear,
+  //   month: filteredMonth,
+  // });
+
+  const filteredEvents = events;
   
   if(!filteredEvents || filteredEvents.length === 0){
    return (
@@ -34,13 +42,46 @@ function FilteredEventsPage() {
    );
   }
   
-  const date = new Date(filteredYear, filteredMonth - 1)
+  const date = new Date(dateFromServer.year, dateFromServer.month - 1)
   return (
     <div>
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </div>
   );
+}
+
+export async function getServerSideProps(context){
+  const {params} = context;
+  const filterData = params.slug;
+  const filteredYear = +filterData[0];
+  const filteredMonth = +filterData[1];
+
+  if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredMonth > 12){
+    return {
+      props:{hasError: true},
+      // notFound: true,
+      // redirect: {
+      //   destination: '/404' // We not handle Error Page
+      // }
+    }
+  }
+
+  const filteredEvents = await getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+
+
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: filteredYear,
+        month: filteredMonth,
+      },
+    },
+  };
 }
 
 export default FilteredEventsPage;
