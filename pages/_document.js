63c70a232488437aebfb8197e3f4ddef667ffa0b
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-09 00:31:03 +0200

    Handle _document.js page

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import Document, {Html, Head, Main, NextScript} from "next/document";
+
+class MyDocument extends Document{
+    render(){
+        return(
+            <Html lang="en">
+                <body>
+                    <div id="overlay" />
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
