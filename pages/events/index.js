c8da2eec3490ce670d0d261488e250deb6c37aa6
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-08 06:09:02 +0200

    All Events Page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
-import {getAllEvents} from '../../dummy-data';
+import {getAllEvents} from '../../helpers/api-util';
 import EventList from "../../components/events/EventList";
 import EventsSearch from '../../components/events/EventsSearch';
 import { useRouter } from 'next/router';
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const {events} = props;
 
   function handleFindEvents(year, month){
     const fullPath = `/events/${year}/${month}`;
@@ -18,6 +18,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps(context){
+  const events = await getAllEvents();
+
+  return{
+    props: {
+      events,
+    },
+    revalidate: 5
+  }
 }
 
 export default AllEventsPage;
