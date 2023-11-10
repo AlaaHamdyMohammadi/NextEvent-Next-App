89c1e95c5ceceaa9b9a25b1d1a448a50f75c451f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-11 01:20:15 +0200

    Handle Comments

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,20 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import axios from 'axios';
 
 function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    if (showComments) {
+      axios.get(`/api/comments/${eventId}`).then((res) => {
+        console.log(res.data.data);
+        setComments(res.data.data);
+      });
+    }
+  }, [showComments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
-  function addCommentHandler(commentData) {
+  async function addCommentHandler(commentData) {
     // send data to API
+    const res = await axios.post(`/api/comments/${eventId}`, commentData, {
+      headers:{
+        "Content-Type": "application/json",
+      }
+    });
   }
 
   return (
@@ -23,7 +39,7 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList items={comments}/>}
     </section>
   );
 }
--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,25 +1,36 @@
-function handler(req, res){
-    if(req.method === 'POST'){
-        const {email, name, comment} = req.body;
-        if(!email.includes('@') || !name || name.trim() === '' || !comment || comment.trim() === ''){
-            res.status(422).json({message: 'Invalid data!'});
-            return;
-        }
-        const newComment = {
-            id: new Date().toISOString(),
-            email,
-            name,
-            comment
-        };
-        console.log(newComment);
-        res.status(201).json({ message: "Data Successfully Added.", data: newComment });
+function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, text } = req.body;
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid data!" });
+      return;
     }
-    if(req.method === 'GET'){
-        const {eventId} = req.query;
-        
-    }
-
-    
-    
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+    console.log(newComment);
+    res
+      .status(201)
+      .json({ message: "Data Successfully Added.", data: newComment });
+  }
+  if (req.method === "GET") {
+    // const {eventId} = req.query;
+    const dummyList = [
+      { id: "c1", name: "Alaa", text: "Comment1" },
+      { id: "c2", name: "Arwa", text: "Comment2" },
+    ];
+    res
+      .status(200)
+      .json({ message: "Data Successfully Added.", data: dummyList });
+  }
 }
 export default handler;
