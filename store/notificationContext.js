63d09d3134e99548e9c3f59f092106ac0a687bcb
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-12 00:52:04 +0200

    Handle Comments

--- store/notificationContext.js
+++ store/notificationContext.js
@@ -1,25 +1,49 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
 
 const NotificationContext = createContext({
-  notification: null,
+  notification: null, // { title, message, status }
   showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
-export function NotificationContextProvider({children}){
-    const [activeNotification, setActiveNotification] = useState();
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
 
-    function handleShowNotification(notificationData){
-        setActiveNotification(notificationData);
-    }
-
-    function handleHideNotification(){
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
         setActiveNotification(null);
-    }
+      }, 3000);
 
-    const context = {notification: activeNotification, showNotification: handleShowNotification, hideNotification: handleHideNotification}
-    return <NotificationContext.Provider value={context}>{children}</NotificationContext.Provider>
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
 }
 
 export default NotificationContext;
